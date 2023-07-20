8b4043c5ee7899bc5a6a5619c742ca316f46dc0f
TladiKekana <tladikarts@gmail.com>
2023-07-20 11:05:58 +0200

    Added Employees Controller

---
+++ EmployeePortal.Web/Controllers/EmployeesController.cs
@@ -0,0 +1,21 @@
+﻿using EmployeePortal.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeePortal.Web.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private readonly IEmployeeData db;
+
+        public EmployeesController(IEmployeeData db)
+        {
+            this.db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var model = db.GetAll();
+            return View(model);
+        }
+    }
+}
